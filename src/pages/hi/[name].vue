95afe621d353c7d3f61c06259a212ffca892ff8a
jiangxd <jiangxd2016@gmail.com>
2023-08-18 11:34:36 +0800

    feat: update deps

--- src/pages/hi/[name].vue
+++ src/pages/hi/[name].vue
@@ -6,20 +6,13 @@ const router = useRouter();
 <template>
   <div>
     <div i-carbon-pedestrian inline-block text-4xl />
-    <p>
-      Hi, {{ props.name }}
-    </p>
+    <p>Hi, {{ props.name }}</p>
     <p text-sm op50>
       <em>Dynamic route!</em>
     </p>
 
     <div>
-      <button
-        class="m-3 mt-8 text-sm btn"
-        @click="router.back()"
-      >
-        Back
-      </button>
+      <button class="m-3 mt-8 text-sm btn" @click="router.back()">Back</button>
     </div>
   </div>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
 import Counter from '@/components/Counter';
 
-const name = $ref('');
+const name = ref('');
 const IndexRef = ref<HTMLElement>();
 
-onMounted(()=>{
+onMounted(() => {
   console.info(IndexRef.value?.innerHTML);
-
 });
 
 const router = useRouter();
 const go = () => {
-  if (name) { router.push(`/hi/${encodeURIComponent(name)}`); }
+  if (name.value) {
+    router.push(`/hi/${encodeURIComponent(name)}`);
+  }
 };
 </script>
 
@@ -20,9 +21,7 @@ const go = () => {
     <div i-carbon-campsite inline-block text-4xl />
     <Counter />
     <p>
-      <a rel="noreferrer" href="" target="_blank">
-        Vite Starter
-      </a>
+      <a rel="noreferrer" href="" target="_blank"> Vite Starter </a>
     </p>
     <p>
       <em text-sm op75>Opinionated Vite Starter Template</em>
@@ -31,15 +30,22 @@ const go = () => {
     <div py-4 />
 
     <input
-      id="input" v-model="name" placeholder="What's your name?" type="text" autocomplete="false" p="x-4 y-2"
-      w="250px" text="center" bg="transparent" border="~ rounded gray-200 dark:gray-700" outline="none active:none"
+      id="input"
+      v-model="name"
+      placeholder="What's your name?"
+      type="text"
+      autocomplete="false"
+      p="x-4 y-2"
+      w="250px"
+      text="center"
+      bg="transparent"
+      border="~ rounded gray-200 dark:gray-700"
+      outline="none active:none"
       @keydown.enter="go"
     />
 
     <div>
-      <button class="m-3 text-sm btn" :disabled="!name" @click="go">
-        Go
-      </button>
+      <button class="m-3 text-sm btn" :disabled="!name" @click="go">Go</button>
     </div>
   </div>
 </template>
